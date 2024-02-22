9934f5e147d4945392fb86124c81a81fce834b4f
devarshi-007 <devarshi.trivedi@improwised.com>
2024-02-22 09:59:38 +0000

    caching updation on user add/removal in user group

--- frappe/core/doctype/user_group/user_group.py
+++ frappe/core/doctype/user_group/user_group.py
@@ -2,14 +2,33 @@
 # License: MIT. See LICENSE
 
 import frappe
-
-# import frappe
+from frappe.core.doctype.user_group_permission.user_group_permission import (
+	get_user_permissions_from_user_group,
+)
 from frappe.model.document import Document
 
 
 class UserGroup(Document):
-	def after_insert(self):
-		frappe.cache().delete_key("user_groups")
+	def on_update(self):
+		old_doc = self.get_doc_before_save()
+		old_users = []
+		for user in old_doc.user_group_members:
+			old_users.append(user.user)
 
-	def on_trash(self):
-		frappe.cache().delete_key("user_groups")
+		new_users = []
+		for user in self.user_group_members:
+			new_users.append(user.user)
+
+		deleted_users, added_users = self.find_modified_users(old_users, new_users)
+
+		for user in deleted_users:
+			frappe.cache().hdel("user_group_permission", user)
+
+		for user in added_users:
+			frappe.cache().hdel("user_group_permission", user)
+			get_user_permissions_from_user_group(user)
+
+	def find_modified_users(self, old_list, new_list):
+		deleted_users = [user for user in old_list if user not in new_list]
+		added_users = [user for user in new_list if user not in old_list]
+		return deleted_users, added_users
